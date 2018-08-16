3419fee31a8fc94897d704d27e5271744cf5ba06
SanketSanath <SanketSanath@users.noreply.github.com>
2018-08-16 19:40:18 +0530

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -14,9 +14,15 @@ app.post('/todos', (req, res)=>{
 
 	todo.save().then((doc)=>{
 		res.send(doc);
-		console.log(e);
 	}, (e)=>{
-		console.log(e);
+		res.status(400).send(e);
+	});
+});
+
+app.get('/todos', (req, res)=>{
+	Todo.find().then((todos)=>{
+		res.send({todos});
+	}, (e)=>{
 		res.status(400).send(e);
 	});
 });
