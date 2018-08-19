b90d5d796cb08ce532460cc8a8b4ba0bf8ab0575
SanketSanath <SanketSanath@users.noreply.github.com>
2018-08-19 14:59:07 +0530

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 const {mongoose, Todo, User} = require('./db/model');
 const {authenticate} = require('./middleware/authenticate');
@@ -108,6 +109,18 @@ app.post('/users', (req, res)=>{
 
 app.get('/users/me', authenticate, (req, res)=>{
 	res.send(req.user);
+});
+
+app.post('/users/login', (req, res)=>{
+	var body = _.pick(req.body, ['email', 'password']);
+
+	User.findByCredentials(body.email, body.password).then((user)=>{
+		return user.generateAuthToken().then((token)=>{
+			res.header('x-auth', token).send(user);
+		});
+	}).catch((e)=>{
+		res.status(400).send('some error occured');
+	})
 });
 
 app.listen(3000, ()=>{
