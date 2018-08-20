7e41fb3ce12f5e87800017f90cbd485f2f993a50
SanketSanath <SanketSanath@users.noreply.github.com>
2018-08-20 16:20:57 +0530

    Add DELETE /users/me/token route

--- server/db/model.js
+++ server/db/model.js
@@ -127,6 +127,16 @@ UserSchema.pre('save', function(next){
 	}
 });
 
+UserSchema.methods.removeToken = function(token){
+	var user = this;
+
+	return user.update({
+		$pull: {
+			tokens : {token}
+		}
+	});
+};
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {
--- server/server.js
+++ server/server.js
@@ -123,6 +123,14 @@ app.post('/users/login', (req, res)=>{
 	})
 });
 
+app.delete('/users/me/token', authenticate, (req, res)=>{
+	req.user.removeToken(req.token).then(()=>{
+		res.status(200).send('successfully loged out');
+	}, (e)=>{
+		res.status(400).send('error');
+	});
+});
+
 app.listen(3000, ()=>{
 	console.log('connected to port 3000');
 });
